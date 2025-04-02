4b9ee260952f25b00c93d66064c4d9375f8c5b55
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-04-02 21:04:50 +0100

    changed some rows to sections in index.html. added some additional styling to slots and tiles. started on the flex properties of the puzzle area

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const story = {
     intro: {
         title: "Introduction",
         storyText: `<p></p>Lila sits on her bed looking at the floor, a curtain of dark hair covering her face. Her soft plushies watch her from their shelves with sad eyes and downturned mouths. Lila doesn't move, not even when her mum pops her head around the doorway and calls her name; gently, carefully, as if anything above a whisper would shatter her fragile daughter. Lila doesn't respond. She can't. The words won't come out.
-The door clicks softly shut and Lila stares at the magnetic board on the wall above her bed. It should say 'Goodnight' but some of the letters have fallen off. Lila picks them up from the bedspread and starts to place them in the empty spots.</p>
+The door clicks softly shut and Lila stares at the magnetic board on the wall above her bed. It should spell a word but the letters have fallen off. Lila picks them up from the bedspread and starts to place them in the empty spots...</p>
 <button id="getPuzzle">Puzzle</button>`
     },
 
@@ -18,37 +18,39 @@ document.getElementById("title").innerText = story[story.currentChapter].title;
 document.getElementById("story-text").innerHTML = story[story.currentChapter].storyText;
 document.getElementById("story-text").classList.add(`chapter-${story.currentChapter}`);
 
+// Used to delay the passing of this function until the DOM has finished loading. Important as the getPuzzle button is added via dynamically modified DOM content.
 setTimeout(() => {
     const puzzleButton = document.getElementById("getPuzzle");
     if (puzzleButton) {
-    puzzleButton.addEventListener("click", puzzle1);
+        puzzleButton.addEventListener("click", puzzle1);
     } else {
         console.error("Button not found!")
     }
 }, 0);
 
-
+// 
 function puzzle1() {
-    console.log("This worked")
     const word = "GOODNIGHT";
     let shuffled = word.split("").sort();
     const puzzleQuestion = document.getElementById("puzzle-question");
     const puzzleAnswer = document.getElementById("puzzle-answer");
 
-    puzzleQuestion.innerHTML= " ";
+    puzzleQuestion.innerHTML = " ";
     puzzleAnswer.innerHTML = " ";
 
-    word.split("").forEach((letter) => {
+    word.split("").forEach(() => {
         let slot = document.createElement("div");
         slot.classList.add("slot");
         puzzleAnswer.appendChild(slot);
+        // slot.addEventListener("click", clickedSlot);
     });
 
     shuffled.forEach((letter) => {
         let tile = document.createElement("div");
         tile.classList.add("tile");
         tile.textContent = letter;
+        tile.setAttribute("data-letter", letter);
         puzzleQuestion.appendChild(tile);
-
+        // tile.addEventListener("click", clickedTile);
     });
 }
