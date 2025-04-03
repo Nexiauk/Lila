f9e34c2a756613328c954b2a28651c05ff96945b
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-04-03 18:59:51 +0100

    Added lila and chapter images. Styled and added media queries for mobile and tablet

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const story = {
         title: "Introduction",
         storyText: `<p></p>Lila sits on her bed looking at the floor, a curtain of dark hair covering her face. Her soft plushies watch her from their shelves with sad eyes and downturned mouths. Lila doesn't move, not even when her mum pops her head around the doorway and calls her name; gently, carefully, as if anything above a whisper would shatter her fragile daughter. Lila doesn't respond. She can't. The words won't come out.
 The door clicks softly shut and Lila stares at the magnetic board on the wall above her bed. It should spell a word but the letters have fallen off. Lila picks them up from the bedspread and starts to place them in the empty spots...</p>
-<button id="getPuzzle">Puzzle</button>`
+<center><button id="getPuzzle">Puzzle</button></center>`
     },
 
     1: {
