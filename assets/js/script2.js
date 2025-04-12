9202ed3bb594d08dba7444223491cbb1ed408377
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-04-12 15:11:57 +0100

    created global variables for chaptersvisited, and for the ending

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -108,6 +108,12 @@ const choice1 = document.getElementById("choice-1");
 const choice2 = document.getElementById("choice-2");
 const choice3 = document.getElementById("choice-3");
 const checkArea = document.getElementById("buttons");
+let ending = false;
+let introVisited = false;
+let voidVisited = false;
+let forestVisited = false;
+let lakeVisited = false;
+let libraryVisited = false;
 
 let word = "";
 
